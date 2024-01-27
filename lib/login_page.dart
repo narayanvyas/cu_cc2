b76174710e2f3b5b790152eec96b83393d196395
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-27 12:56:31 +0530

    Working on Login Page;

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+
+bool validateForm() {
+  if (!RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(emailController.text)) {
+    print('Email is wrong');
+    return false;
+  } else if (passwordController.text.length < 6) {
+    print('Password is wrong');
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void performLogin() {
+  if (validateForm()) {
+    print('Login Success');
+  }
+}
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -14,32 +37,39 @@ class LoginPage extends StatelessWidget {
         backgroundColor: Colors.black,
         centerTitle: true,
       ),
-      body: ListView(
-        padding: EdgeInsets.only(left: 30, right: 30, top: 30),
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Email',
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 30, right: 30),
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
             ),
-          ),
-          Container(
-            height: 30,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Password',
+            Container(
+              height: 30,
             ),
-          ),
-          Container(
-            height: 30,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Login Now'),
-          )
-        ],
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                performLogin();
+              },
+              child: Text('Login Now'),
+            )
+          ],
+        ),
       ),
     );
   }
